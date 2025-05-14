8dbc1edf0363442a5cb9a2f1e550dcef3473063a
Abdul Rehman <158306204+Abdul-Rehman6@users.noreply.github.com>
2025-05-14 17:59:14 +0500

    Fix Tailwind CSS oxide addon loading error

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
+  define: {
+    'process.env.TAILWIND_MODE': JSON.stringify('build'),
+  },
   plugins: [
     react(),
     svgr({
